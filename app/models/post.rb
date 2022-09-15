5c828ba4efdb81e332686da39ce748caf7fe5e2a
Andyveloper <mcantillo.gdi@gmail.com>
2022-09-15 17:26:48 -0500

    Create models and associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ApplicationRecord
+  has_many :likes
+  has_many :comments
+  belongs_to :author, class_name: 'User'
+
+  def self.update_post_counter(user)
+    user.post_counter = Post.where(author: user).count
+  end
+
+  after_save :update_post_counter
+
+  private
+
+  def update_post_counter
+    author.increment!(:post_counter)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  has_many :comments, inverse_of: 'author'
+  has_many :posts, inverse_of: 'author'
+  has_many :likes, inverse_of: 'author'
+  def most_recent
+    Post.where(author_id).limit(3).order(created_at: :desc)
+  end
 end
