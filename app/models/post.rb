b4bc2de707bf2173f6a6d4de8d638eba2dd2587f
Andyveloper <mcantillo.gdi@gmail.com>
2022-09-15 17:56:31 -0500

    Improved Post auto updating counter

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,10 +3,6 @@ class Post < ApplicationRecord
   has_many :comments, inverse_of: 'post'
   belongs_to :author, class_name: 'User'
 
-  def self.update_post_counter(user)
-    user.post_counter = Post.where(author: user).count
-  end
-
   after_save :update_post_counter
 
   private
