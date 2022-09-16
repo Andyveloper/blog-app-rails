c7bd8f6a3b61dd3f7143c4f86cecee447a5b1fcf
Andyveloper <mcantillo.gdi@gmail.com>
2022-09-16 18:40:49 -0500

    Create unit testing for like and comment

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,4 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
+  new_post = Post.create(title: 'Testing unit', author: User.create(name: 'David'))
+  other_user = User.create(name: 'Elson')
+  subject { Like.create(author: other_user, post: new_post) }
+
+  it 'should updated the like counter' do
+    expect(subject.post.likes_counter).to be 1
+    Like.create(author: subject.author, post: subject.post)
+    expect(subject.post.likes_counter).to be 2
+  end
 end
