408af4efe476638de24321e526d718f9bf6e0584
Andyveloper <mcantillo.gdi@gmail.com>
2022-09-16 18:23:58 -0500

    Create test units for User and Post

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,17 @@
 class User < ApplicationRecord
   has_many :comments, foreign_key: 'author_id'
+
   has_many :posts, foreign_key: 'author_id'
+
   has_many :likes, foreign_key: 'author_id'
 
+  after_initialize do |user|
+    user.post_counter = 0
+  end
+
   validates :name, presence: true
-  validates :post_counter, presence: true, only_integer: true, greater_than_or_equal_to: 0
+
+  validates :post_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def most_recent
     posts.order(created_at: :desc).limit(3)
