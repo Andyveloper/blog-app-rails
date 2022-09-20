9eb6c6a57d5ed316e9de3ae448465d34fbc8f343
Andyveloper <mcantillo.gdi@gmail.com>
2022-09-20 18:32:42 -0500

    Finish test units for show

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -15,4 +15,19 @@ RSpec.describe 'Users', type: :request do
       expect(response.body).to include('Find me in app/views/users/index.html.erb')
     end
   end
+  describe 'GET /show' do
+    before(:each) do
+      get user_path(id: 1)
+    end
+    it 'returns http success' do
+      get '/users/show'
+      expect(response).to have_http_status(:success)
+    end
+    it 'returns correct template' do
+      expect(response).to render_template(:show)
+    end
+    it 'returns correct body placeholder' do
+      expect(response.body).to include('Find me in app/views/users/show.html.erb')
+    end
+  end
 end
