0fa81a9f734564717b94cbf76eaebca4c45934d6
Andyveloper <mcantillo.gdi@gmail.com>
2022-09-20 18:30:55 -0500

    Create users spec file and tests for index

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'Users', type: :request do
+  describe 'GET /index' do
+    before(:each) do
+      get users_path
+    end
+    it 'returns http success' do
+      expect(response).to have_http_status(:success)
+    end
+    it 'returns correct rendering' do
+      expect(response.body).to render_template(:index)
+    end
+    it 'returns correct body placeholder' do
+      expect(response.body).to include('Find me in app/views/users/index.html.erb')
+    end
   end
 end
