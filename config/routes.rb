ef8eea6422e84b6dc0170bd0ba66b2ae85730dc2
Andyveloper <mcantillo.gdi@gmail.com>
2022-09-22 18:04:26 -0500

    Create show html structure

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show]
+    resources :posts, only: %i[index show] do
+      resources :comments, only: [:new, :create]
+    end
   end
-
   # Defines the root path route ("/")
   root 'users#index'
 end
