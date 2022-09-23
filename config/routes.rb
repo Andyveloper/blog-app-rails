c7013e9d8958b9d1b5528d097533f4c245df0139
Andyveloper <mcantillo.gdi@gmail.com>
2022-09-23 18:21:54 -0500

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :users, only: %i[index show] do
     resources :posts, only: %i[index show] do
-      resources :comments, only: [:new, :create]
+      resources :comments, only: %i[new create]
     end
   end
   # Defines the root path route ("/")
