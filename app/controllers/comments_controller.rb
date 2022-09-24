c65d406548271594a5d91544ba52d3270b67b7e7
Andyveloper <mcantillo.gdi@gmail.com>
2022-09-24 03:07:32 -0500

    Change controllers

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,20 @@
 class CommentsController < ApplicationController
-  def new; end
+  def create
+    author = current_user
+    post = Post.find(params[:post_id])
+    comment = Comment.new(params.require(:comment).permit(:author, :post, :text))
+    comment.author = author
+    comment.post = post
 
-  def create; end
+    respond_to do |format|
+      format.html do
+        if comment.save
+          flash[:success] = "Comment created successfully"
+          redirect_to user_post_url(author, post)
+        else
+          flash.now[:error] = "Comment not created"
+        end
+      end
+    end
+  end
 end
