39e8326ed8205dad98f9fcdcae7d14e8bd1855dd
Andyveloper <mcantillo.gdi@gmail.com>
2022-09-27 16:27:13 -0500

    Set up devise and migrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'likes/create'
   resources :users, only: %i[index show] do
     resources :posts, only: %i[index show new create] do
       resources :comments, only: %i[create]
