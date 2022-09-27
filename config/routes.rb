963c3fa52de6abeb918e28409badf23ac183fc8d
Andyveloper <mcantillo.gdi@gmail.com>
2022-09-27 18:45:47 -0500

    Fix sign up form and enable devise path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: ""
   resources :users, only: %i[index show] do
     resources :posts, only: %i[index show new create] do
       resources :comments, only: %i[create]
