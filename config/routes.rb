17b58340a32957ea0b2780eef2c27dac9ca42bd3
Andyveloper <mcantillo.gdi@gmail.com>
2022-09-29 17:17:42 -0500

    Add endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,19 @@ Rails.application.routes.draw do
   devise_for :users, path: ""
   resources :users, only: %i[index show] do
     resources :posts do
-      resources :comments, only: %i[create destroy]
+      resources :comments, only: %i[index create destroy]
       resources :likes, only: %i[create]
     end
   end
   # Defines the root path route ("/")
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:show] do
+        resources :posts, only: [:index, :show] do
+          resources :comments, only: [:create, :index]
+        end
+      end
+    end
+  end 
   root 'users#index'
 end
