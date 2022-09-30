3c35ccb35b45b05b06ac81661efc9991eeb848d0
Andyveloper <mcantillo.gdi@gmail.com>
2022-09-30 17:06:12 -0500

    Set Auth parameters for users

--- spec/requests/api/v1/authentication_spec.rb
+++ spec/requests/api/v1/authentication_spec.rb
@@ -1,17 +1,31 @@
 require 'swagger_helper'
 
 RSpec.describe 'api/v1/authentication', type: :request do
-
+  before(:all) do
+    @user = User.create(
+      name: "サトシ", 
+      email: "testingapi@gmail.com", 
+      password: "123456", 
+      password_confirmation: "123456"
+    )
+  end
   
   path '/api/v1/login' do
 
     post('Login Authentication') do
+      tags 'User'
+      consumes 'application/json'
+      parameter name: :user, in: :body,
+      schema: {
+        type: :object, 
+        properties: { 
+          email: { type: :string }, 
+          password: { type: :string } 
+        },
+         required: ['email', 'password']
+      }
 
-      parameter :email
-      parameter :password
-
-      response(200, 'Successful') do
-        let(:user) {{ email: "testingapi@gmail.com", password: "123456" } }
+      response '200', 'Successful' do
         after do |example|
           example.metadata[:response][:content] = {
             'application/json' => {
@@ -20,8 +34,11 @@ RSpec.describe 'api/v1/authentication', type: :request do
           }
         end
           run_test!
-        end
+      end
+      response '401', 'Unauthorized' do
+        let(:user) {{ email: "testingapi@gmail.com", password: "555000" } }
+        run_test!
       end
     end
   end
-
+end
