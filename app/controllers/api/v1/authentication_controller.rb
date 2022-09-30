77435347596c3a2abfbecf362f84e93a5a630b33
David Vera Castillo <indigodavid@gmail.com>
2022-09-29 19:31:06 -0500

    Fix login method when validating password"

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -1,12 +1,12 @@
 class Api::V1::AuthenticationController < ApplicationController
-  skip_before_action :authenticate_request
+  skip_before_action :verify_authenticity_token
 
   include JsonWebToken
   def login
     @user = User.find_by_email(params[:email])
-    if @user&.authenticate(params[:password])
+    if @user.valid_password? params[:password]
       token = jwt_encode(user_id: @user.id)
-      render json: { token: token }, status: :ok
+      render json: { token: }, status: :ok
     else
       render json: { error: 'Unauthorized' }, status: :unauthorized
     end
